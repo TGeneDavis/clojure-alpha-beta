7d364b46da33bfae3648e4c82c78873d318a2d27
TGeneDavis <wxdavishome@gmail.com>
2017-11-07 05:40:15 -0700

    Meta data telling source of choice is being tested to 3 plies.

--- test/clojure_alpha_beta/alpha_beta_tests.clj
+++ test/clojure_alpha_beta/alpha_beta_tests.clj
@@ -43,16 +43,25 @@
 ;;lambdas for the init
 
 (defn my-finder
+  "Take a node and generate all child nodes ... ((node)(node)(node)) ... with new metadata :source"
   [x] 
   (if (seq? x)
-    x
+    (do
+      (let [x-count (count x)
+            new-x (map 
+                    #(with-meta %1 {:source (str (:source (meta x)) "," %2)}) 
+                    x
+                    (iterate inc 0))]        
+        new-x))
     nil))
 
 (defn my-scorer
+  "Create a '(number) score with attached metadata :source"
   [x]
   x)
 
 (defn my-terminal?
+  "Determine if the given node is a leaf node ... like no king"
   [x] 
   (number?
     (first x)))
@@ -95,12 +104,16 @@
       finder     ;;lazy-branch-finder
       scorer     ;;leaf-score-finder
       terminal?) ;;terminal
-  
     
-    (is (= 1 (first (ab/initial-call
-                      '((5) (7) (1))         ;;origin-node
+    (def root-node (with-meta '((5) (7) (1)) {:source "root"}))
+    
+    (def best-score (ab/initial-call
+                      root-node ;;origin-node
                       1         ;;depth
-                      false)))))) ;;is-alpha))
+                      false))    ;;is-alpha
+    
+    (is (= 1 (first best-score))) ;;is-alpha
+    (is (= "root,2" (:source (meta best-score)))))) ;;is-alpha
 
 
 (deftest one-ply-max-tree
@@ -113,12 +126,17 @@
       finder     ;;lazy-branch-finder
       scorer     ;;leaf-score-finder
       terminal?) ;;terminal
-  
+
+        
+    (def root-node (with-meta '((5) (7) (1)) {:source "root"}))
     
-    (is (= 7 (first (ab/initial-call
-                      '((5) (7) (1))         ;;origin-node
+    (def best-score (ab/initial-call
+                      root-node ;;origin-node
                       1         ;;depth
-                      true)))))) ;;is-alpha))
+                      true))    ;;is-alpha
+    
+    (is (= 7 (first best-score))) ;;is-alpha
+    (is (= "root,1" (:source (meta best-score)))))) ;;is-alpha
           
 
 (deftest two-max-ply-tree
@@ -131,12 +149,17 @@
       finder     ;;lazy-branch-finder
       scorer     ;;leaf-score-finder
       terminal?) ;;terminal
-  
+
+    (def root-node (with-meta '(((6) (3) (16)) ((-17) (10) (-5)) ((-14) (-10) (-18))) {:source "root"}))
     
-    (is (= 3 (first (ab/initial-call
-                      '(((6) (3) (16)) ((-17) (10) (-5)) ((-14) (-10) (-18)))         ;;origin-node
+    (def best-score (ab/initial-call
+                      root-node ;;origin-node
                       2         ;;depth
-                      true)))))) ;;is-alpha))
+                      true))    ;;is-alpha
+    
+    (is (= 3 (first best-score))) ;;is-alpha
+    (is (= "root,0,1" (:source (meta best-score)))))) ;;is-alpha
+    
           
 
 (deftest two-min-ply-tree
@@ -149,12 +172,16 @@
       finder     ;;lazy-branch-finder
       scorer     ;;leaf-score-finder
       terminal?) ;;terminal
-  
+
+    (def root-node (with-meta '(((6) (3) (16)) ((-17) (10) (-5)) ((-14) (-10) (-18))) {:source "root"}))
     
-    (is (= -10 (first (ab/initial-call
-                        '(((6) (3) (16)) ((-17) (10) (-5)) ((-14) (-10) (-18)))         ;;origin-node
-                        2         ;;depth
-                        false)))))) ;;is-alpha))
+    (def best-score (ab/initial-call
+                      root-node ;;origin-node
+                      2         ;;depth
+                      false))    ;;is-alpha
+    
+    (is (= -10 (first best-score))) ;;is-alpha
+    (is (= "root,2,1" (:source (meta best-score)))))) ;;is-alpha
           
 
 (deftest three-max-ply-tree
@@ -167,20 +194,20 @@
       finder     ;;lazy-branch-finder
       scorer     ;;leaf-score-finder
       terminal?) ;;terminal
-  
+
+    (def root-node (with-meta 
+                     '((((6)(-4)(-14))((16)(-7)(15))((-3)(1)(1)))(((8)(1)(6))((13)(-14)(-10))((-8) (-17) (-17)))(((5)(5)(17))((6)(-14)(16))((-2)(-1)(5))))  ;; origin node 
+                     {:source "root"}))
     
-    (is (= 5 (first (ab/initial-call
-                      '((((6) (-4) (-14)) 
-                         ((16) (-7) (15)) 
-                         ((-3) (1) (1)))
-                        (((8) (1) (6))
-                         ((13) (-14) (-10))
-                         ((-8) (-17) (-17)))
-                        (((5) (5) (17))
-                         ((6) (-14) (16))
-                         ((-2) (-1) (5))))  ;; origin node 
-                       3                    ;;depth
-                       true))))))           ;;is-alpha
+    (def best-score (ab/initial-call
+                      root-node ;;origin-node
+                      3         ;;depth
+                      true))    ;;is-alpha
+    
+    (comment Note that there are actually several 5's that could have been remembered, 
+      but the last one is the one used.)
+    (is (= 5 (first best-score))) ;;is-alpha
+    (is (= "root,2,2,2" (:source (meta best-score)))))) ;;is-alpha
           
           
 
@@ -194,22 +221,30 @@
       finder     ;;lazy-branch-finder
       scorer     ;;leaf-score-finder
       terminal?) ;;terminal
-  
+
+    (def root-node (with-meta 
+                     '((((6)(-4)(-14))((16)(-7)(15))((-3)(1)(1)))
+                       (((8)(1)(6))((13)(-14)(-10))((-8) (-17) (-17)))
+                       (((5)(5)(17))((6)(-14)(16))((-2)(-1)(5))))  ;; origin node 
+                     {:source "root"}))
     
-    (is (= -3 (first (ab/initial-call
-                       '((((6) (-4) (-14))
-                          ((16) (-7) (15)) 
-                          ((-3) (1) (1)))
-                       
-                         (((8) (1) (6))
-                          ((13) (-14) (-10))
-                          ((-8) (-17) (-17)))
-                       
-                         (((5) (5) (17))
-                          ((6) (-14) (16))
-                          ((-2) (-1) (5))))  ;; origin node 
-                        3                    ;;depth
-                        false))))))          ;;is-alpha
+    (def best-score (ab/initial-call
+                      root-node ;;origin-node
+                      3         ;;depth
+                      false))    ;;is-alpha
+    
+    (comment Note that there are actually several 5's that could have been remembered, 
+      but the last one is the one used.)
+    (is (= -3 (first best-score))) ;;is-alpha
+    (is (= "root,0,2,0" (:source (meta best-score)))) ;;is-alpha
+   
+    
+    (comment is (= -3 (first (ab/initial-call
+                               '((((6)(-4)(-14))((16)(-7)(15))((-3)(1)(1)))
+                                 (((8)(1)(6))((13)(-14)(-10))((-8) (-17) (-17)))
+                                 (((5)(5)(17))((6)(-14)(16))((-2)(-1)(5))))  ;; origin node 
+                               3                    ;;depth
+                               false))))))          ;;is-alpha
 
 
 
