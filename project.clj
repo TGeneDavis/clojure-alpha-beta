eed187314963d1b44367bd1bd58a0554c491a21d
T. Gene Davis <tgenedavis@gmail.com>
2021-12-21 09:35:04 -0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clojure_alpha_beta "0.2.0"
-  :description "Generic Alpha-Beta Pruning library written in Clojure and ClojureScript for my https://birdshogi.com/ site."
+  :description "Generic Alpha-Beta Pruning library written in Clojure and ClojureScript."
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :aot [clojure-alpha-beta.core]
